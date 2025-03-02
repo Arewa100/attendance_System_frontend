a89017fb13aa7d9955fbaedfb676e56474f1ba61
Arewa100 <olasorexway@gmail.com>
2025-03-02 12:23:17 +0100

    landing page update

--- src/components/homepage.jsx
+++ src/components/homepage.jsx
@@ -1,3 +1,4 @@
+import Footer from "./footer"
 import NavBar from "./navBar"
 
 
@@ -5,8 +6,20 @@ const Home = ()=> {
      
     return(
         <>
-        <div className="mobile-screen: bg-pink-300 h-[1000px] xl:max-w-[1440px] m-auto">
+        <div className="xl:max-w-[1440px] m-auto min-h-screen flex flex-col">
             <NavBar/>
+            <section className="flex-grow relative">
+                <div className="h-full w-full relative flex items-center justify-around min-h-screen">
+        
+                    <div className="absolute inset-0 bg-[url('src/assets/images/backImg.svg')] bg-center bg-no-repeat bg-[length:400px_400px] opacity-10 z-0"></div>
+            
+                    <div className="relative z-10 w-[750px] h-[87px]">
+                        <h1 className="font-extralight text-[25px] text-center text-[#1E1E1E]">"Effortlessly track student attendance and access detailed attendance history. Streamline your record-keeping and ensure accuracy with ease."</h1>
+                    </div>
+                </div>
+            </section>
+
+            <Footer/>
         </div>
         </>
     )
--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -7,12 +7,16 @@ const NavBar = ()=> {
                 <div className="xl: h-[9px] tablets: max-w-full mobile-screen: bg-white "></div> {/* //headline */}
 
                 <div className=" mobile-screen:h-[40px] tablets:h-full flex justify-between items-center mobile-screen:max-w-full">
-                    <div className="bg-red-500 flex justify-between items-center  mobile-screen:h-full mobile-screen:ml-0 tablets:ml-[40px] gap-4">
-                        <img className="mobile-screen:h-10 tablets:h-[101px]" src="src/assets/images/logo.svg" alt="yabatech logo" />
-                        <div className="text-[24px] text-[#FDC800] font-roboto">Yabatech Student Attendance...</div>
+                    <div className="flex justify-between items-center  mobile-screen:h-full mobile-screen:ml-0 tablets:ml-[40px] gap-4">
+                        <img className="mobile-screen:h-10 tablets:h-[63px] " src="src/assets/images/logo.svg" alt="yabatech logo" />
+                        <div className="tablets:text-[24px] mobile-screen:text-[10px] text-[#FDC800] font-roboto ">Yabatech Student Attendance...</div>
                     </div>
-                    {/* i am here  */}
-                    <div className="bg-slate-500 w-[300px] h-full flex items-center"><Button textContent="Login" style="bg-red-400"/></div>  
+    
+                    <div className=" tablets:w-[300px] tablets:h-full tablets:flex tablets:items-center tablets:gap-[20px] mobile-screen:hidden">
+                        <Button textContent="Login" style="h-[42px] w-[100px] bg-[#FDC800] text-[17px] text-[#3A1B1B] font-roboto rounded-[12px] shadow-sm font-medium hover:bg-[#1F3A1F] hover:text-[#FDC800] transition duration-[0.1s]"/>
+                        <Button textContent="Sign Up" style="h-[42px] w-[100px] bg-[#1F3A1F] text-[17px] text-[#FDC800] font-roboto rounded-[12px] shadow-sm font-medium hover:bg-[#FDC800] hover:text-[#1F3A1F] transition duration-[0.1s]"/>
+
+                    </div>  
                 </div>
     
         </div>
