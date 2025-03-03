f2a9e65eb04082388cabaf7873cf594a3d650863
Arewa100 <olasorexway@gmail.com>
2025-03-03 01:34:01 +0100

    login page and signup page updated

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,9 +1,19 @@
 import Home from "../components/homepage";
+import LoginPage from "../components/loginpage";
+import SignUpPage from "../components/signup";
 
 const routes = [
     {
         path:"/",
         element: <Home/>
+    },
+    {
+        path:"/login",
+        element: <LoginPage/>
+    },
+    {
+        path:"/signup",
+        element: <SignUpPage/>
     }
 ]
 
