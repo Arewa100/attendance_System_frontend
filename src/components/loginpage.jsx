772fdf4d33840048dd90b2afce48377ddffd791b
Arewa100 <olasorexway@gmail.com>
2025-03-03 02:28:36 +0100

    login and signup

--- src/components/loginpage.jsx
+++ src/components/loginpage.jsx
@@ -1,7 +1,14 @@
 import Button from "../resusables/button"
 import { Link } from "react-router-dom"
+import { useNavigate } from "react-router-dom"
+import { motion } from "framer-motion"
+
 const LoginPage = ()=> {
 
+    let navigate = useNavigate();
+    const handleCancelButton = ()=> {
+        navigate("/");
+    }
     return(
         <>
           <div className="xl:max-w-[1440px] m-auto min-h-screen flex items-center justify-around">
@@ -16,21 +23,21 @@ const LoginPage = ()=> {
 
                             <div className="h-6 w-full mt-2">
 
-                                <button className="ml-[570px] text-[#FDC800]">
+                                <motion.button whileTap={{scale:0.2}} onClick={handleCancelButton} className="ml-[570px] text-[#FDC800]">
                                     <svg className="h-6 w-6 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
                                         <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
                                     </svg>
-                                </button>
+                                </motion.button>
 
                             </div>
 
-                            <div className="h-10 w-full flex justify-around text-[#FDC800] font-medium text-[36px] font-roboto"><p>Login</p></div>
+                            <div className="h-10 w-full flex justify-around text-[#FDC800] font-medium text-[25px] font-roboto"><p>Login</p></div>
                             <div  className="h-[140px] mt-8 w-full flex flex-col justify-between items-center">
                                 <input className="w-[520px] h-[40px] rounded-md p-4 outline-none font-roboto shadow-md bg-[rgba(246,238,238,0.1)] text-purple-200" type="text" placeholder="Enter Username"/>
                                 <input className="w-[520px] h-[40px] rounded-md p-4 outline-none font-roboto shadow-md bg-[rgba(246,238,238,0.1)] text-purple-200" type="password" placeholder="Enter Password" />
                             </div>
                             <div className="h-[42px] w-full mt-10 flex justify-around items-center">
-                                <Button textContent="Login" style="h-[42px] w-[150px] bg-[#FDC800] text-[17px] text-[#1F3A1F] font-roboto rounded-[12px] shadow-sm font-medium hover:bg-[#1F3A1F] hover:text-[#FDC800] transition duration-[0.1s] shadow-md"/>
+                                <Button textContent="Login" style=" outline-none h-[42px] w-[150px] bg-[#FDC800] text-[17px] text-[#1F3A1F] font-roboto rounded-[12px] shadow-sm font-medium hover:bg-[#1F3A1F] hover:text-[#FDC800] transition duration-[0.1s] shadow-md"/>
                             </div>
 
                             <div className="flex justify-around items-center mt-7">
--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -1,6 +1,17 @@
 import Button from "../resusables/button"
+import { useNavigate } from "react-router-dom"
 
 const NavBar = ()=> {
+
+    let navigate = useNavigate();
+
+    const handleLoginClick = ()=> {
+       navigate("login");
+    }
+    const handleSignUpClick = ()=> {
+        navigate("signup");
+    }
+
     return(
         <>
         <div className="mobile-screen:bg-[#006600] tablets:h-[136px] pt-9 mobile-screen:max-w-full">
@@ -9,12 +20,12 @@ const NavBar = ()=> {
                 <div className=" mobile-screen:h-[40px] tablets:h-full flex justify-between items-center mobile-screen:max-w-full">
                     <div className="flex justify-between items-center  mobile-screen:h-full mobile-screen:ml-0 tablets:ml-[40px] gap-4">
                         <img className="mobile-screen:h-10 tablets:h-[63px] " src="src/assets/images/logo.svg" alt="yabatech logo" />
-                        <div className="tablets:text-[24px] mobile-screen:text-[10px] text-[#FDC800] font-roboto ">Yabatech Student Attendance...</div>
+                        <div className="  tablets:text-[24px] mobile-screen:text-[10px] text-[#FDC800] font-roboto ">Yabatech Student Attendance...</div>
                     </div>
     
                     <div className=" tablets:w-[300px] tablets:h-full tablets:flex tablets:items-center tablets:gap-[20px] mobile-screen:hidden">
-                        <Button textContent="Login" style="h-[42px] w-[100px] bg-[#FDC800] text-[17px] text-[#3A1B1B] font-roboto rounded-[12px] shadow-sm font-medium hover:bg-[#1F3A1F] hover:text-[#FDC800] transition duration-[0.1s]"/>
-                        <Button textContent="Sign Up" style="h-[42px] w-[100px] bg-[#1F3A1F] text-[17px] text-[#FDC800] font-roboto rounded-[12px] shadow-sm font-medium hover:bg-[#FDC800] hover:text-[#1F3A1F] transition duration-[0.1s]"/>
+                        <Button textContent="Login" style=" h-[42px] w-[100px] bg-[#FDC800] text-[17px] text-[#3A1B1B] font-roboto rounded-[12px] shadow-sm font-medium hover:bg-[#1F3A1F] hover:text-[#FDC800] transition duration-[0.1s]" onClick={handleLoginClick}/>
+                        <Button textContent="Sign Up" style="  h-[42px] w-[100px] bg-[#1F3A1F] text-[17px] text-[#FDC800] font-roboto rounded-[12px] shadow-sm font-medium hover:bg-[#FDC800] hover:text-[#1F3A1F] transition duration-[0.1s] " onClick={handleSignUpClick}/>
 
                     </div>  
                 </div>
