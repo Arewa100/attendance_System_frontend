dbefcb981429d6c1c1d09719d96399af89d10bbc
Arewa100 <olasorexway@gmail.com>
2025-06-02 02:31:25 +0100

    new footer update

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -2,7 +2,7 @@ const Footer = ()=> {
     return(
         <>
         <div className="mobile-screen:max-w-full tablets:h-[87px] mobile-screen:bg-[#006600] flex items-center justify-around">
-            <h2 className="text-[#D9D9D9] font-light font-roboto">@ 2025 Designed and Developed by Olasoyin Miracle</h2>
+            <h2 className="text-[#D9D9D9] font-light font-roboto">@ 2025 Designed and Developed by Computer Science department</h2>
         </div>
         </>
     )
